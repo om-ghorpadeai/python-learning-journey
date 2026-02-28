c8b2b9bb927b7958e7a5c8969cf72337b53c408c
om ghorpade <omghorpadeceo@gmail.com>
2026-02-28 23:03:46 +0530

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,10 @@
+# Day 3 - User Input in Python
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+goal = input("Enter your goal: ")
+
+print("\nWelcome", name)
+print("You are", age, "years old")
+print("Your goal is to become", goal)
+print("You will achieve it soon. Keep working daily.")
