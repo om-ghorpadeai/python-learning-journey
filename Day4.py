55e6f60b382338eaf1e01d384c35e52f2aac50bd
om ghorpade <omghorpadeceo@gmail.com>
2026-03-01 23:41:44 +0530

    Create Day4.py

---
+++ Day4.py
@@ -0,0 +1,12 @@
+# Day 4 - If Else in Python
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult.")
+    print("You can achieve your dream of becoming AI Engineer.")
+else:
+    print("You are young.")
+    print("Start learning now and you will become AI Engineer soon.")
+
+print("Keep working daily, Om.")
