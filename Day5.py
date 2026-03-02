4f2c9721f3f7e62fd594b1f4cb8ad989188808e5
om ghorpade <omghorpadeceo@gmail.com>
2026-03-02 19:43:38 +0530

    Add Day 5 Python loops example

---
+++ Day5.py
@@ -0,0 +1,11 @@
+# Day 5 - Loops in Python
+
+print("Printing numbers from 1 to 5:")
+
+for i in range(1, 6):
+    print(i)
+
+print("\nNow printing your goal 5 times:")
+
+for i in range(5):
+    print("I will become an AI Engineer")
