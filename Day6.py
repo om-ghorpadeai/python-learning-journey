4e57a94f35e9f4635e90eeffcc2570d55710503d
om ghorpade <omghorpadeceo@gmail.com>
2026-03-03 23:17:26 +0530

    Add user-controlled countdown program

---
+++ Day6.py
@@ -0,0 +1,11 @@
+# Day 6 - User Controlled Countdown
+
+print("=== Countdown Program ===")
+
+number = int(input("Enter a starting number: "))
+
+while number > 0:
+    print("Countdown:", number)
+    number -= 1
+
+print("Blast off! 🚀")
